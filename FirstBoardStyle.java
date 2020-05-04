66bcac6018bfb295e7ba43fe9d6fa0b90cf5dc7f
sushil-7808 <57238655+sushil-7808@users.noreply.github.com>
2020-05-04 13:13:21 -0700

    Add files via upload

---
+++ FirstBoardStyle.java
@@ -0,0 +1,81 @@
+package com.company;
+
+import java.awt.Color;
+
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.plaf.metal.MetalLookAndFeel;
+import javax.swing.plaf.metal.OceanTheme;
+
+
+/**
+ * First board style is subclass of Board Style implementing the methods defined in
+ * the BoardStyle
+
+ * @author Admin
+ *
+ */
+public class FirstBoardStyle implements BoardStyle {
+
+    /**
+     * The board color is defined in this attribute.
+     */
+    public Color btnBgColor=Color.PINK;
+
+
+
+    /**
+     * This method will be implemented as part of MVC mode.
+     * The main goal of this method is to capture user events on gui
+     * After capturing the events on gui this method is responsible to repaint
+     * with the required changes.
+     * The doOperation invoked from TicTacToeBoard context  dynamically during runtime..
+     */
+    @Override
+    public void doOperation() {
+        // TODO Auto-generated method stub
+
+    }
+
+
+    /**
+     * The style of this board is chosen to be pink as defined above in btnBgColor.
+     * The getBtnColoris invoked from TicTacToeBoard context  dynamically during runtime..
+     *
+     */
+    @Override
+    public Color getBtnColor() {
+        // TODO Auto-generated method stub
+        return btnBgColor;
+    }
+
+
+
+    /**
+     * FirstBoardStyle has chosen to use Ocean Theme
+     * The below method sets the ocean theme in UIManager.
+     * When the board is created the board uses OceanTheme style.
+     * The setBoardTheme invoked from TicTacToeBoard context  dynamically during runtime..
+     */
+
+    @Override
+    public void setBoardTheme() {
+        MetalLookAndFeel.setCurrentTheme(new OceanTheme());
+        try {
+           // UIManager.setLookAndFeel(new MetalLookAndFeel());
+            UIManager.setLookAndFeel( UIManager.getCrossPlatformLookAndFeelClassName() );
+        } catch (UnsupportedLookAndFeelException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+}
---
+++ TicTacToeBoard.java
@@ -0,0 +1,143 @@
+package com.company;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import javax.swing.plaf.metal.DefaultMetalTheme;
+import javax.swing.plaf.metal.MetalLookAndFeel;
+import javax.swing.plaf.metal.OceanTheme;
+
+public class TicTacToeBoard extends JFrame implements ItemListener, ActionListener{
+    Checkbox strategy1,strategy2;
+    Icon ic1,ic2,icon,ic11,ic22;
+    boolean state,type,set;
+
+    //Strategy type identified from GUI, this is associated with radio button.
+    String strategyType;
+
+    //Strategy that shall be used will be decided during runtime
+    BoardStyle boardStrategy;
+
+    //This is not used , will be used in MVC implementation
+    private static String synthFile = "buttonSkin.xml";
+
+
+    /**
+     * This is constuctor of this class
+     * This is invoked from StrategyPatternDemo
+     * This constructor creates the board in GUI .
+     */
+
+    TicTacToeBoard(){
+        super("TIC TAC TOE - GAME");
+
+        CheckboxGroup cbg=new CheckboxGroup();
+        strategy1=new Checkbox("Board Style 1",cbg,false);
+        strategy2=new Checkbox("Board Style 2",cbg,false);
+        strategy1.setBounds(120,80,200,40);
+        strategy2.setBounds(120,150,200,40);
+        add(strategy1); add(strategy2);
+        strategy1.addItemListener(this);
+        strategy2.addItemListener(this);
+
+
+        state=true;type=true;set=true;
+        ic1=new ImageIcon("ic1.jpg");
+        ic2=new ImageIcon("ic2.jpg");
+        ic11=new ImageIcon("ic11.jpg");
+        ic22=new ImageIcon("ic22.jpg");
+
+        setLayout(null);
+        setSize(330,450);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    }//eof constructor
+
+
+
+    /**
+     * This method is implementation of ActionListener interface extended by this class.
+     *
+     *This method is dummy method and shall be used during MVC implementation
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    }
+    /**
+     *
+     * This method is the implementation of the ItemListener.
+     * This method is invoked when the user selects radio button based on style requirement.
+     *
+     */
+    @Override
+    public void itemStateChanged(ItemEvent e) {
+        if (strategy1.getState()) {
+            System.out.println(strategy1.getState());
+            strategyType="style1";
+            boardStrategy=new FirstBoardStyle();
+
+        }else if (strategy2.getState()) {
+
+            boardStrategy=new SecondBoardStyle();
+        }
+        boardStrategy.doOperation();
+        remove(strategy1);
+        remove(strategy2);
+        repaint(0, 0, 330, 450);
+
+
+        displayBoard();
+    }
+    /**
+     * This method is called from itemStateChanged after user selects the board style.
+     * This is the context where Strategy is implemented during runtime
+     * During runtime user can select any style required ,based on it the method
+     * changes the behavior of the board with invoking the corresponding strategy implementation
+     * IF the user chooses first style - FirstBoardStyle is used
+     * IF the user chooses second style - SecondBoardStyle is used
+     * This method is used to display the board with updated behavior provided by strategy.
+     */
+    public void displayBoard() {
+        int i, j, ii, jj, x, y, yesnull;
+        JButton b[] = new JButton[9];
+        JButton reset;
+        x = 10;
+        y = 10;
+        j = 0;
+        for (i = 0; i <= 8; i++, x += 100, j++) {
+            b[i] = new JButton();
+            Border line = new LineBorder(Color.BLACK);
+
+            Border margin = new EmptyBorder(5, 15, 5, 15);
+            Border compound = new CompoundBorder(line, margin);
+            b[i].setBorder(compound);
+            b[i].setOpaque(true);
+            b[i].setBackground(boardStrategy.getBtnColor());
+           // b[i].setBackground(Color.ORANGE);
+
+            if (j == 3) {
+                j = 0;
+                y += 100;
+                x = 10;
+            }
+            b[i].setBounds(x, y, 100, 100);
+            add(b[i]);
+            b[i].addActionListener(this);
+        } // eof for
+
+        reset = new JButton("RESET");
+        reset.setBounds(100, 350, 100, 50);
+        add(reset);
+        reset.addActionListener(this);
+
+    }// eof showButton
+
+
+
+}
+
+
